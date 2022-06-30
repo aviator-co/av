d8722ff156cf67935349ee4a89ef26b1e2c829c5
Ankit Jain <ankit@aviator.co>
2022-06-30 13:14:14 -0700

    [MER-873] Highlight current branch in the stack

--- cmd/av/stack_tree.go
+++ cmd/av/stack_tree.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/aviator-co/av/internal/meta"
-	"github.com/spf13/cobra"
+	"os"
 	"strings"
+
+	"github.com/aviator-co/av/internal/git"
+	"github.com/aviator-co/av/internal/meta"
+	"github.com/aviator-co/av/internal/utils/colors"
+	"github.com/spf13/cobra"
 )
 
 var stackTreeCmd = &cobra.Command{
@@ -26,32 +30,49 @@ var stackTreeCmd = &cobra.Command{
 			return err
 		}
 
+		currentBranch, err := repo.CurrentBranchName()
+		if err != nil {
+			return err
+		}
+
 		// TODO[polish]:
 		// 		We should show information about whether or not each branch is
 		//     	up-to-date with its stack parent as well as whether or not the
 		//		branch is up-to-date with its upstream tracking branch.
-		fmt.Println(defaultBranch)
+		if currentBranch == defaultBranch {
+			_, _ = fmt.Fprint(os.Stderr,
+				colors.Success("* "), colors.Success(defaultBranch), "\n",
+			)
+		} else {
+			fmt.Println(defaultBranch)
+		}
 		for branch, branchMeta := range branches {
 			if branchMeta.Parent != "" {
 				// not a stack root
 				continue
 			}
-			printStackTree(branches, branch, 1)
+			printStackTree(branches, repo, currentBranch, branch, 1)
 		}
 
 		return nil
 	},
 }
 
-func printStackTree(branches map[string]meta.Branch, root string, depth int) {
+func printStackTree(branches map[string]meta.Branch, repo *git.Repo, currentBranch string, root string, depth int) {
 	indent := strings.Repeat("    ", depth)
 	branch, ok := branches[root]
 	if !ok {
 		fmt.Printf("%s<ERROR: unknown branch: %s>\n", indent, root)
 		return
 	}
-	_, _ = fmt.Printf("%s%s\n", indent, branch.Name)
+	if currentBranch == branch.Name {
+		_, _ = fmt.Fprint(os.Stderr,
+			indent, colors.Success("* "), colors.Success(branch.Name), "\n",
+		)
+	} else {
+		_, _ = fmt.Printf("%s%s\n", indent, branch.Name)
+	}
 	for _, next := range branch.Children {
-		printStackTree(branches, next, depth+1)
+		printStackTree(branches, repo, currentBranch, next, depth+1)
 	}
 }
