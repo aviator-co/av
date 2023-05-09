aviator-co/av

commit 3ef3a1668bc7dae5b7028d1500051e4098c0cfa2

fix: Fix --trunk so that it actually rebases to the upstream trunk (#103)
Without this fix, the test fails.

internal/actions/sync_branch.go
@@ -139,13 +139,48 @@ func syncBranchRebase(
 		return nil, errors.WrapIff(err, "failed to get head of branch %q", branch.Name)
 	}
 
-	if branch.IsStackRoot() {
-		trunk := branch.Parent.Name
-		if !opts.ToTrunk {
+	parentState := branch.Parent
+	parentBranch, _ := tx.Branch(parentState.Name)
+	origParent := branch.Parent
+	origParentBranch := parentBranch
+	for parentBranch.MergeCommit != "" {
+		parentState = parentBranch.Parent
+		parentBranch, _ = tx.Branch(parentState.Name)
+	}
+
+	if parentState.Trunk {
+		var newUpstreamCommitHash string
+		if opts.ToTrunk {
+			// First, try to fetch latest commit from the trunk...
+			_, _ = fmt.Fprint(os.Stderr,
+				"  - fetching latest commit from ", colors.UserInput("origin/", parentState.Name), "\n",
+			)
+			if _, err := repo.Run(&git.RunOpts{
+				Args: []string{"fetch", "origin", parentState.Name},
+			}); err != nil {
+				_, _ = fmt.Fprint(os.Stderr,
+					"  - ",
+					colors.Failure("error: failed to fetch HEAD of "), colors.UserInput(parentState.Name),
+					colors.Failure(" from origin: ", err.Error()), "\n",
+				)
+				return nil, errors.WrapIff(err, "failed to fetch trunk branch %q from origin", parentState.Name)
+			}
+
+			// NOTE: Strictly speaking, if a user doesn't use the default refspec (e.g. fetch is
+			// not +refs/heads/*:refs/remotes/origin/*, the remote tracking branch is not
+			// origin/$TRUNK. As we just fetched from a remote, it'd be safe to use FETCH_HEAD.
+			trunkHead, err := repo.RevParse(&git.RevParse{Rev: "origin/" + parentState.Name})
+			if err != nil {
+				return nil, errors.WrapIff(err, "failed to get HEAD of %q", parentState.Name)
+			}
+			newUpstreamCommitHash = trunkHead
+		} else if origParentBranch.MergeCommit != "" {
+			newUpstreamCommitHash = origParentBranch.MergeCommit
+		} else {
 			_, _ = fmt.Fprint(os.Stderr,
 				"  - branch is a stack root, nothing to do",
 				" (run ", colors.CliCmd("av stack sync --trunk"),
-				" to sync against the latest commit in ", colors.UserInput(trunk), ")\n",
+				" to sync against the latest commit in ", colors.UserInput(parentState.Name), ")\n",
 			)
 			return &SyncBranchResult{
 				git.RebaseResult{Status: git.RebaseAlreadyUpToDate},
@@ -153,38 +188,22 @@ func syncBranchRebase(
 				branch,
 			}, nil
 		}
-
-		// First, try to fetch latest commit from the trunk...
-		_, _ = fmt.Fprint(os.Stderr,
-			"  - fetching latest commit from ", colors.UserInput("origin/", trunk), "\n",
-		)
-		if _, err := repo.Run(&git.RunOpts{
-			Args: []string{"fetch", "origin", trunk},
-		}); err != nil {
-			_, _ = fmt.Fprint(os.Stderr,
-				"  - ",
-				colors.Failure("error: failed to fetch HEAD of "), colors.UserInput(trunk),
-				colors.Failure(" from origin: ", err.Error()), "\n",
-			)
-			return nil, errors.WrapIff(err, "failed to fetch trunk branch %q from origin", trunk)
-		}
-
-		// NOTE: Strictly speaking, if a user doesn't use the default refspec (e.g. fetch is
-		// not +refs/heads/*:refs/remotes/origin/*, the remote tracking branch is not
-		// origin/$TRUNK. As we just fetched from a remote, it'd be safe to use FETCH_HEAD.
-		trunkHead, err := repo.RevParse(&git.RevParse{Rev: "origin/" + trunk})
-		if err != nil {
-			return nil, errors.WrapIff(err, "failed to get HEAD of %q", trunk)
-		}
-
 		rebase, err := repo.RebaseParse(git.RebaseOpts{
 			Branch:   opts.Branch,
-			Upstream: trunkHead,
+			Upstream: origParent.Head,
+			Onto:     newUpstreamCommitHash,
 		})
 		if err != nil {
 			return nil, err
 		}
 		msgRebaseResult(rebase)
+
+		if !origParent.Trunk {
+			_, err = syncBranchUpdateNewTrunk(tx, branch, parentState.Name)
+			if err != nil {
+				return nil, err
+			}
+		}
 		//nolint:exhaustive
 		switch rebase.Status {
 		case git.RebaseConflict:
@@ -210,12 +229,11 @@ func syncBranchRebase(
 	//      happen if the parent branch was rebased itself).
 
 	// Scenario 1: the parent branch has been merged.
-	parent, _ := tx.Branch(branch.Parent.Name)
-	if parent.MergeCommit != "" {
-		short := git.ShortSha(parent.MergeCommit)
+	if parentBranch.MergeCommit != "" {
+		short := git.ShortSha(parentBranch.MergeCommit)
 		_, _ = fmt.Fprint(os.Stderr,
 			"  - parent ", colors.UserInput(branch.Parent.Name),
-			" (pull ", colors.UserInput("#", parent.PullRequest.GetNumber()), ")",
+			" (pull ", colors.UserInput("#", parentBranch.PullRequest.GetNumber()), ")",
 			" was merged\n",
 		)
 		_, _ = fmt.Fprint(os.Stderr,
@@ -253,7 +271,7 @@ func syncBranchRebase(
 			//                  X'--Y'  stacked-2
 			// Note that we've introduced B into the history of stacked-2, but
 			// not C or D since those commits come after M.
-			Onto: parent.MergeCommit,
+			Onto: parentBranch.MergeCommit,
 		})
 		if err != nil {
 			return nil, err
@@ -263,14 +281,14 @@ func syncBranchRebase(
 				*rebase,
 				&SyncBranchContinuation{
 					OldHead:      branchHead,
-					ParentCommit: parent.MergeCommit,
-					NewTrunk:     parent.Parent.Name,
+					ParentCommit: parentBranch.MergeCommit,
+					NewTrunk:     parentBranch.Parent.Name,
 				},
 				branch,
 			}, nil
 		}
 
-		branch, err = syncBranchUpdateNewTrunk(tx, branch, parent.Parent.Name)
+		branch, err = syncBranchUpdateNewTrunk(tx, branch, parentState.Name)
 		if err != nil {
 			return nil, err
 		}
@@ -278,19 +296,19 @@ func syncBranchRebase(
 	}
 
 	// Scenario 2: the branch is up-to-date with its parent.
-	parentHead, err := repo.RevParse(&git.RevParse{Rev: parent.Name})
+	parentHead, err := repo.RevParse(&git.RevParse{Rev: parentState.Name})
 	if err != nil {
-		return nil, errors.WrapIff(err, "failed to resolve HEAD of parent branch %q", parent.Name)
+		return nil, errors.WrapIff(err, "failed to resolve HEAD of parent branch %q", parentState.Name)
 	}
 	mergeBase, err := repo.MergeBase(&git.MergeBase{
 		Revs: []string{parentHead, branch.Name},
 	})
 	if err != nil {
-		return nil, errors.WrapIff(err, "failed to compute merge base of %q and %q", parent.Name, branch.Name)
+		return nil, errors.WrapIff(err, "failed to compute merge base of %q and %q", parentState.Name, branch.Name)
 	}
 	if mergeBase == parentHead {
 		_, _ = fmt.Fprint(os.Stderr,
-			"  - already up-to-date with parent ", colors.UserInput(parent.Name),
+			"  - already up-to-date with parent ", colors.UserInput(parentState.Name),
 			"\n",
 		)
 		return &SyncBranchResult{git.RebaseResult{Status: git.RebaseAlreadyUpToDate}, nil, branch}, nil
@@ -300,7 +318,7 @@ func syncBranchRebase(
 	_, _ = fmt.Fprint(os.Stderr,
 		"  - synching branch ", colors.UserInput(branch.Name),
 		" on latest commit ", git.ShortSha(parentHead),
-		" of parent branch ", colors.UserInput(parent.Name),
+		" of parent branch ", colors.UserInput(parentState.Name),
 		"\n",
 	)
 	// We need to use `rebase --onto` here and be very careful about how we

e2e_tests/stack_sync_trunk_test.go
@@ -1,0 +1,88 @@
+package e2e_tests
+
+import (
+	"testing"
+
+	"github.com/aviator-co/av/internal/git"
+	"github.com/aviator-co/av/internal/git/gittest"
+	"github.com/aviator-co/av/internal/meta/jsonfiledb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStackSyncTrunk(t *testing.T) {
+	repo := gittest.NewTempRepo(t)
+	Chdir(t, repo.Dir())
+
+	// To start, we create a simple two-stack where each stack has a single commit.
+	// Our stack looks like:
+	//     main:    X
+	//     stack-1:  \ -> 1a -> 1b
+	//     stack-2:              \ -> 2a -> 2b
+	require.Equal(t, 0, Cmd(t, "git", "checkout", "-b", "stack-1").ExitCode)
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n"), gittest.WithMessage("Commit 1a"))
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n"), gittest.WithMessage("Commit 1b"))
+	RequireAv(t, "stack", "branch", "stack-2")
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n"), gittest.WithMessage("Commit 2a"))
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n2b\n"), gittest.WithMessage("Commit 2b"))
+
+	// Everything up to date now, so this should be a no-op.
+	require.Equal(t, 0, Av(t, "stack", "sync", "--no-fetch", "--no-push").ExitCode)
+
+	// We simulate a merge here so that our history looks like:
+	//     main:    X --------------> 1S -> 3a
+	//     stack-1:  \ -> 1a -> 1b
+	//     stack-2:              \ -> 2a -> 2b
+	// where 1S is the squash-merge commit of 1b onto main. Note that since it's
+	// a squash commit, 1S is not a *merge commit* in the Git definition.
+	var squashCommit string
+	var threeACommit string
+	gittest.WithCheckoutBranch(t, repo, "main", func() {
+		RequireCmd(t, "git", "merge", "--squash", "stack-1")
+		// `git merge --squash` doesn't actually create the commit, so we have to
+		// do that separately.
+		RequireCmd(t, "git", "commit", "--no-edit")
+
+		var err error
+		squashCommit, err = repo.RevParse(&git.RevParse{Rev: "HEAD"})
+		require.NoError(t, err, "failed to get squash commit")
+
+		gittest.CommitFile(t, repo, "test-file", []byte("3a\n"), gittest.WithMessage("Commit 3a"))
+		threeACommit , err = repo.RevParse(&git.RevParse{Rev: "HEAD"})
+		require.NoError(t, err, "failed to get squash commit")
+
+		RequireCmd(t, "git", "push", "origin", "main")
+	})
+
+	// We shouldn't do this as part of an E2E test since it depends on internal
+	// knowledge of the codebase, but :shrug:. We need to set the merge commit
+	// manually since we can't actually communicate with the GitHub API as part
+	// of this test.
+	db, err := jsonfiledb.OpenRepo(repo)
+	require.NoError(t, err, "failed to open repo db")
+	tx := db.WriteTx()
+	stack1Meta, _ := tx.Branch("stack-1")
+	stack1Meta.MergeCommit = squashCommit
+	tx.SetBranch(stack1Meta)
+	require.NoError(t, tx.Commit())
+
+	require.Equal(t, 0,
+		Cmd(t, "git", "merge-base", "--is-ancestor", "stack-1", "stack-2").ExitCode,
+		"HEAD of stack-1 should be an ancestor of HEAD of stack-2 before running sync",
+	)
+	require.NotEqual(t, 0,
+		Cmd(t, "git", "merge-base", "--is-ancestor", squashCommit, "stack-2").ExitCode,
+		"squash commit of stack-1 should not be an ancestor of HEAD of stack-2 before running sync",
+	)
+
+	RequireAv(t, "stack", "sync", "--no-fetch", "--no-push", "--trunk")
+	// At this point, the stack should be:
+	//
+	//     main:    X --------------> 1S -> 3a
+	//     stack-1:  \ -> 1a -> 1b           \
+	//     stack-2:                           \ -> 2a -> 2b
+
+	require.Equal(t, 0,
+		Cmd(t, "git", "merge-base", "--is-ancestor", threeACommit, "stack-2").ExitCode,
+		"commit 3a should be an ancestor of HEAD of stack-2 after running sync with --trunk",
+	)
+}

internal/git/gittest/repo.go
@@ -3,6 +3,7 @@ package gittest
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/aviator-co/av/internal/git"
@@ -19,19 +20,34 @@ func init() {
 // NewTempRepo initializes a new git repository with reasonable defaults.
 func NewTempRepo(t *testing.T) *git.Repo {
 	var dir string
+	var remoteDir string
 	if os.Getenv("AV_TEST_PRESERVE_TEMP_REPO") != "" {
 		var err error
 		dir, err = os.MkdirTemp("", "repo")
 		require.NoError(t, err)
 		logrus.Infof("created git test repo: %s", dir)
+
+		remoteDir, err = os.MkdirTemp("", "remote-repo")
+		require.NoError(t, err)
+		logrus.Infof("created git remote test repo: %s", remoteDir)
 	} else {
-		dir = t.TempDir()
+		dir = filepath.Join(t.TempDir(), "local")
+		require.NoError(t, os.MkdirAll(dir, 0755))
+
+		remoteDir = filepath.Join(t.TempDir(), "remote")
+		require.NoError(t, os.MkdirAll(remoteDir, 0755))
 	}
 	init := exec.Command("git", "init", "--initial-branch=main")
 	init.Dir = dir
 
 	err := init.Run()
 	require.NoError(t, err, "failed to initialize git repository")
+
+	remoteInit := exec.Command("git", "init", "--bare")
+	remoteInit.Dir = remoteDir
+
+	err = remoteInit.Run()
+	require.NoError(t, err, "failed to initialize remote git repository")
 
 	repo, err := git.OpenRepo(dir)
 	require.NoError(t, err, "failed to open repo")
@@ -45,7 +61,7 @@ func NewTempRepo(t *testing.T) *git.Repo {
 		require.NoErrorf(t, err, "failed to set config %s=%s", k, v)
 	}
 
-	_, err = repo.Git("remote", "add", "origin", "git@github.com:aviator-co/nonexistent-repo.git", "--master=main")
+	_, err = repo.Git("remote", "add", "origin", remoteDir, "--master=main")
 	require.NoError(t, err, "failed to set remote")
 
 	err = os.WriteFile(dir+"/README.md", []byte("# Hello World"), 0644)
@@ -56,6 +72,9 @@ func NewTempRepo(t *testing.T) *git.Repo {
 
 	_, err = repo.Git("commit", "-m", "Initial commit")
 	require.NoError(t, err, "failed to create initial commit")
+
+	_, err = repo.Git("push", "origin", "main")
+	require.NoError(t, err, "failed to push to remote")
 
 	db, err := jsonfiledb.OpenRepo(repo)
 	require.NoError(t, err, "failed to open database")

commit dadcfa6729c5ab6ac09dab21761f2668b9f17442

fix: Fix av CLI not re-parenting children (#107)
I think this bug was introduced in #103. I see how it happened and it suggests to me that the code is a little bit hard to follow at the moment. There's room to clean it up so that bugs like this don't reoccur. I don't think the version of `av` with this bug present has been released.

The test I added fails without this change (and I noticed this issue locally).

internal/actions/sync_branch.go
@@ -229,11 +229,11 @@ func syncBranchRebase(
 	//      happen if the parent branch was rebased itself).
 
 	// Scenario 1: the parent branch has been merged.
-	if parentBranch.MergeCommit != "" {
-		short := git.ShortSha(parentBranch.MergeCommit)
+	if origParentBranch.MergeCommit != "" {
+		short := git.ShortSha(origParentBranch.MergeCommit)
 		_, _ = fmt.Fprint(os.Stderr,
 			"  - parent ", colors.UserInput(branch.Parent.Name),
-			" (pull ", colors.UserInput("#", parentBranch.PullRequest.GetNumber()), ")",
+			" (pull ", colors.UserInput("#", origParentBranch.PullRequest.GetNumber()), ")",
 			" was merged\n",
 		)
 		_, _ = fmt.Fprint(os.Stderr,
@@ -241,14 +241,14 @@ func syncBranchRebase(
 			" on top of merge commit ", colors.UserInput(short), "\n",
 		)
 		if opts.Fetch {
-			if _, err := repo.Git("fetch", "origin", branch.MergeCommit); err != nil {
+			if _, err := repo.Git("fetch", "origin", origParentBranch.MergeCommit); err != nil {
 				return nil, errors.WrapIff(err, "failed to fetch merge commit %q from origin", short)
 			}
 		}
 
 		rebase, err := repo.RebaseParse(git.RebaseOpts{
 			Branch:   branch.Name,
-			Upstream: branch.Parent.Name,
+			Upstream: branch.Parent.Head,
 			// Replay the commits from this branch directly onto the merge commit.
 			// The HEAD of trunk might have moved forward since this, but this is
 			// probably the best thing to do here (we bias towards introducing as
@@ -271,7 +271,7 @@ func syncBranchRebase(
 			//                  X'--Y'  stacked-2
 			// Note that we've introduced B into the history of stacked-2, but
 			// not C or D since those commits come after M.
-			Onto: parentBranch.MergeCommit,
+			Onto: origParentBranch.MergeCommit,
 		})
 		if err != nil {
 			return nil, err
@@ -288,7 +288,33 @@ func syncBranchRebase(
 			}, nil
 		}
 
-		branch, err = syncBranchUpdateNewTrunk(tx, branch, parentState.Name)
+		/*
+			TODO:
+				This assumes that the parent was always merged into trunk. We
+				might want to support the case where a parent branch was actually
+				merged into it's parent (possibly on accident).
+				For example, if we have
+					main -- A -- B -- C
+				and B is merged into A, then we'd want to have a history like
+					main -- A -- C
+				(currently we would consider A the trunk branch for C).
+		*/
+		trunk, ok := meta.Trunk(tx, branch.Parent.Name)
+		if !ok {
+			defaultBranch, err := repo.DefaultBranch()
+			if err != nil {
+				return nil, err
+			}
+			_, _ = fmt.Fprint(os.Stderr,
+				colors.Warning("  - Unable to determine trunk branch of "),
+				colors.UserInput(branch.Parent.Name),
+				colors.Warning(". Assuming "),
+				colors.UserInput(defaultBranch),
+				colors.Warning(" for the new trunk.\n"),
+			)
+			trunk = branch.Parent.Name
+		}
+		branch, err = syncBranchUpdateNewTrunk(tx, branch, trunk)
 		if err != nil {
 			return nil, err
 		}

e2e_tests/stack_sync_merge_commit_test.go
@@ -1,6 +1,8 @@
 package e2e_tests
 
 import (
+	"github.com/aviator-co/av/internal/meta"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/aviator-co/av/internal/git"
@@ -17,28 +19,32 @@ func TestStackSyncMergeCommit(t *testing.T) {
 	// Our stack looks like:
 	//     stack-1: main -> 1a -> 2b
 	//     stack-2:                \ -> 2a -> 2b
+	//     stack-3:			                   \ -> 3a -> 3b
 	require.Equal(t, 0, Cmd(t, "git", "checkout", "-b", "stack-1").ExitCode)
 	gittest.CommitFile(t, repo, "my-file", []byte("1a\n"), gittest.WithMessage("Commit 1a"))
 	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n"), gittest.WithMessage("Commit 1b"))
 	RequireAv(t, "stack", "branch", "stack-2")
 	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n"), gittest.WithMessage("Commit 2a"))
 	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n2b\n"), gittest.WithMessage("Commit 2b"))
+	RequireAv(t, "stack", "branch", "stack-3")
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n2b\n3a\n"), gittest.WithMessage("Commit 3a"))
+	gittest.CommitFile(t, repo, "my-file", []byte("1a\n1b\n2a\n2b\n3a\n3b\n"), gittest.WithMessage("Commit 3b"))
 
 	// Everything up to date now, so this should be a no-op.
 	require.Equal(t, 0, Av(t, "stack", "sync", "--no-fetch", "--no-push").ExitCode)
 
 	// We simulate a merge here so that our history looks like:
-	//     main:    X            / -> 1S
-	//     stack-1:  \ -> 1a -> 2b
+	//     main:    X                         / -> 1S
+	//     stack-1:  \ -> 1a -> 2b           /
 	//     stack-2:              \ -> 2a -> 2b
-	// where 1S is the squash-merge commit of 2b onto main. Note that since it's
-	// a squash commit, 1S is not a *merge commit* in the Git definition.
+	// where 2S is the squash-merge commit of 2b onto main. Note that since it's
+	// a squash commit, 2S is not a *merge commit* in the Git definition.
 	var squashCommit string
 	gittest.WithCheckoutBranch(t, repo, "main", func() {
 		oldHead, err := repo.RevParse(&git.RevParse{Rev: "HEAD"})
 		require.NoError(t, err, "failed to get HEAD")
 
-		RequireCmd(t, "git", "merge", "--squash", "stack-1")
+		RequireCmd(t, "git", "merge", "--squash", "stack-2")
 		// `git merge --squash` doesn't actually create the commit, so we have to
 		// do that separately.
 		RequireCmd(t, "git", "commit", "--no-edit")
@@ -54,24 +60,32 @@ func TestStackSyncMergeCommit(t *testing.T) {
 	db, err := jsonfiledb.OpenRepo(repo)
 	require.NoError(t, err, "failed to open repo db")
 	tx := db.WriteTx()
-	stack1Meta, _ := tx.Branch("stack-1")
-	stack1Meta.MergeCommit = squashCommit
-	tx.SetBranch(stack1Meta)
+	stack2Meta, _ := tx.Branch("stack-2")
+	stack2Meta.MergeCommit = squashCommit
+	tx.SetBranch(stack2Meta)
 	require.NoError(t, tx.Commit())
 
 	require.Equal(t, 0,
-		Cmd(t, "git", "merge-base", "--is-ancestor", "stack-1", "stack-2").ExitCode,
+		Cmd(t, "git", "merge-base", "--is-ancestor", "stack-2", "stack-3").ExitCode,
 		"HEAD of stack-1 should be an ancestor of HEAD of stack-2 before running sync",
 	)
 	require.NotEqual(t, 0,
-		Cmd(t, "git", "merge-base", "--is-ancestor", squashCommit, "stack-2").ExitCode,
+		Cmd(t, "git", "merge-base", "--is-ancestor", squashCommit, "stack-3").ExitCode,
 		"squash commit of stack-1 should not be an ancestor of HEAD of stack-1 before running sync",
 	)
 
 	RequireAv(t, "stack", "sync", "--no-fetch", "--no-push")
 
-	require.Equal(t, 0,
-		Cmd(t, "git", "merge-base", "--is-ancestor", squashCommit, "stack-2").ExitCode,
-		"squash commit of stack-1 should be an ancestor of HEAD of stack-1 after running sync",
+	assert.Equal(t, 0,
+		Cmd(t, "git", "merge-base", "--is-ancestor", squashCommit, "stack-3").ExitCode,
+		"squash commit of stack-2 should be an ancestor of HEAD of stack-3 after running sync",
+	)
+	assert.Equal(t,
+		meta.BranchState{
+			Name:  "main",
+			Trunk: true,
+		},
+		GetStoredParentBranchState(t, repo, "stack-3"),
+		"stack-3 should be re-rooted onto main",
 	)
 }