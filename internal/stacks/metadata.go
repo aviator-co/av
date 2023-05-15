76702112bc69bde32d0e576a68e1218759b5f8f6
Masaya Suzuki <draftcode@gmail.com>
2023-05-15 14:45:51 -0700

    refactor: Remove unused stacks
    It's confusing, but this SyncBranch is different from
    internal/actions/sync_branch.go.

--- internal/stacks/metadata.go
+++
@@ -1,9 +0,0 @@
-package stacks
-
-import (
-	"github.com/aviator-co/av/internal/meta"
-)
-
-// BranchMetadata is metadata about a given branch.
-// TODO: Delete this type alias and just use meta.Branch.
-type BranchMetadata = meta.Branch
