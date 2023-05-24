43f585131af500b4a2be5cac0c23fbc3d5a600de
Kevin Cho <ohcnivek@gmail.com>
2023-05-24 13:52:38 -0400

    feat: Adding av commit create (#122)
    ### feature: av commit create
    <img width="752" alt="Screen Shot 2023-05-19 at 3 11 01 PM" src="https://github.com/aviator-co/av/assets/64056046/830e9acc-5f95-4bbd-b09b-585ca00cf378">
    
    - need to/ will add more flags on top of this pr, but this is the scaffolding for the commit create cmd

---
+++ cmd/av/commit_create.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"emperror.dev/errors"
+	"github.com/aviator-co/av/internal/actions"
+	"github.com/aviator-co/av/internal/config"
+	"github.com/aviator-co/av/internal/git"
+	"github.com/aviator-co/av/internal/meta"
+	"github.com/aviator-co/av/internal/utils/colors"
+	"github.com/spf13/cobra"
+)
+
+var commitCreateFlags struct {
+	// The commit message.
+	Message string
+
+	// Same as `git commit --all`.
+	All bool
+}
+
+
+var commitCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create a commit",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
+
+		currentBranchName, err := repo.CurrentBranchName()
+		if err != nil {
+			return errors.WrapIf(err, "failed to determine current branch")
+		}
+
+		if err := commitCreate(repo, currentBranchName, commitCreateFlags); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	commitCreateCmd.Flags().StringVarP(&commitCreateFlags.Message, "message", "m", "", "the commit message")
+	commitCreateCmd.Flags().BoolVarP(&commitCreateFlags.All, "all", "a", false, "automatically stage modified files (same as git commit --all)")
+}
+
+ func commitCreate(repo *git.Repo, currentBranchName string, flags struct{Message string; All bool}) error {
+	commitArgs := []string{"commit"}
+	if commitCreateFlags.All {
+		commitArgs = append(commitArgs, "--all")
+	}
+	if commitCreateFlags.Message != "" {
+		commitArgs = append(commitArgs, "--message", commitCreateFlags.Message)
+	}
+
+	if _, err := repo.Run(&git.RunOpts{
+		Args:        commitArgs,
+		ExitError:   true,
+		Interactive: true,
+	}); err != nil {
+		_, _ = fmt.Fprint(os.Stderr,
+			"\n", colors.Failure("Failed to create commit."), "\n",
+		)
+		return actions.ErrExitSilently{ExitCode: 1}
+	}
+
+	state, err := actions.ReadStackSyncState(repo)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	ctx := context.Background()
+	db, err := getDB(repo)
+	if err != nil {
+		return err
+	}
+	tx := db.WriteTx()
+	defer tx.Abort()
+
+	client, err := getClient(config.Av.GitHub.Token)
+	if err != nil {
+		return err
+	}
+
+	branchesToSync, err := meta.SubsequentBranches(tx, currentBranchName)
+	if err != nil {
+		return err
+	}
+
+	err = actions.SyncStack(ctx, repo, client, tx, branchesToSync, state, syncFlags)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
