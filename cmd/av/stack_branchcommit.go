fa3a06711f783128cedcd12a85ca7dd91f811663
Travis DePrato <773453+travigd@users.noreply.github.com>
2023-05-25 16:44:38 -0700

    feat(stack branch-commit): Differentiate -a/-A flags (#133)
    Fixes #129.
    
    Technically (but not practically) breaking: `--all` is now equivalent to `av stack branch-commit -A` (before it was equivalent to `-a`).

--- cmd/av/stack_branchcommit.go
+++ cmd/av/stack_branchcommit.go
@@ -42,8 +42,13 @@ var stackBranchCommitFlags struct {
 	// Name of the new branch.
 	BranchName string
 
-	// Same as `git commit --all`.
+	// Same as `git add --all`.
+	// Stages all changes, including untracked files.
 	All bool
+
+	// Same as `git commit --all`.
+	// Stage all files that have been modified and deleted, but ignore untracked files.
+	AllModified bool
 }
 
 var stackBranchCommitCmd = &cobra.Command{
@@ -159,10 +164,29 @@ var stackBranchCommitCmd = &cobra.Command{
 			tx.SetBranch(parentMeta)
 		}
 
-		commitArgs := []string{"commit"}
+		// For "--all" and "--all-modified",
+		var addArgs []string
 		if stackBranchCommitFlags.All {
-			commitArgs = append(commitArgs, "--all")
+			addArgs = append(addArgs, "--all")
+		} else if stackBranchCommitFlags.AllModified {
+			// This is meant to mirror `git commit --all` which does not add
+			// unstaged files to the index (which is different from `git add --all`).
+			addArgs = append(addArgs, "--update")
 		}
+		if len(addArgs) > 0 {
+			_, err := repo.Run(&git.RunOpts{
+				Args:      append([]string{"add"}, addArgs...),
+				ExitError: true,
+			})
+			if err != nil {
+				_, _ = fmt.Fprint(os.Stderr,
+					"\n", colors.Failure("Failed to stage files: ", err.Error()), "\n",
+				)
+				return actions.ErrExitSilently{ExitCode: 1}
+			}
+		}
+
+		commitArgs := []string{"commit"}
 		if stackBranchCommitFlags.Message != "" {
 			commitArgs = append(commitArgs, "--message", stackBranchCommitFlags.Message)
 		}
@@ -192,7 +216,10 @@ var stackBranchCommitCmd = &cobra.Command{
 func init() {
 	stackBranchCommitCmd.Flags().StringVarP(&stackBranchCommitFlags.Message, "message", "m", "", "the commit message")
 	stackBranchCommitCmd.Flags().StringVarP(&stackBranchCommitFlags.BranchName, "branch-name", "b", "", "the branch name to create (if empty, automatically generated from the message)")
-	stackBranchCommitCmd.Flags().BoolVarP(&stackBranchCommitFlags.All, "all", "a", false, "automatically stage modified files (same as git commit --all)")
+	stackBranchCommitCmd.Flags().BoolVarP(&stackBranchCommitFlags.All, "all", "A", false, "automatically stage all files")
+	stackBranchCommitCmd.Flags().BoolVarP(&stackBranchCommitFlags.AllModified, "all-modified", "a", false, "automatically stage modified and deleted files (ignore untracked files)")
+
+	stackBranchCommitCmd.MarkFlagsMutuallyExclusive("all", "all-modified")
 }
 
 func branchNameFromMessage(message string) string {
