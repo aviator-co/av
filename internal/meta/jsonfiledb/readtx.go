c0ef02d55d5a17546f449ac8a446611c90c5899e
Travis DePrato <travis@mergequeue.com>
2023-05-31 11:17:00 -0700

    Remove unused code

--- internal/meta/jsonfiledb/readtx.go
+++ internal/meta/jsonfiledb/readtx.go
@@ -3,7 +3,6 @@ package jsonfiledb
 import (
 	"github.com/aviator-co/av/internal/meta"
 	"github.com/aviator-co/av/internal/utils/maputils"
-	"golang.org/x/exp/slices"
 )
 
 type readTx struct {
@@ -26,19 +25,4 @@ func (tx *readTx) Branch(name string) (branch meta.Branch, ok bool) {
 
 func (tx *readTx) AllBranches() map[string]meta.Branch {
 	return maputils.Copy(tx.state.BranchState)
-}
-
-func (tx *readTx) Children(name string) []meta.Branch {
-	var children []meta.Branch
-	for _, branch := range tx.state.BranchState {
-		if branch.Parent.Name == name {
-			children = append(children, branch)
-		}
-	}
-
-	// Sort for deterministic output.
-	slices.SortFunc(children, func(a, b meta.Branch) bool {
-		return a.Name < b.Name
-	})
-	return children
 }
