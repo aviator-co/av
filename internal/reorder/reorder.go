c53d729f412289fc9f70f89be15158fbecf50f31
Travis DePrato <773453+travigd@users.noreply.github.com>
2023-05-31 11:21:39 -0700

    feat(stack reorder): Implement internal Reorder API (#141)

--- internal/reorder/reorder.go
+++ internal/reorder/reorder.go
@@ -1,7 +1,34 @@
 package reorder
 
-type Opts struct{}
+import (
+	"emperror.dev/errors"
+	"fmt"
+	"github.com/aviator-co/av/internal/utils/colors"
+	"os"
+)
 
-func Reorder(opts Opts) error {
-	panic("not implemented")
+// Reorder executes a reorder.
+// If the reorder couldn't be completed (due to a conflict), a continuation is returned.
+// If the reorder was completed successfully, a nil continuation and nil error is returned.
+func Reorder(ctx Context) (*Continuation, error) {
+	if ctx.Output == nil {
+		ctx.Output = os.Stderr
+	}
+
+	for _, cmd := range ctx.State.Commands {
+		err := cmd.Execute(&ctx)
+		if errors.Is(err, ErrInterruptReorder) {
+			return &Continuation{State: ctx.State}, nil
+		} else if err != nil {
+			return nil, err
+		}
+		ctx.State.Commands = ctx.State.Commands[1:]
+	}
+
+	_, _ = fmt.Fprint(ctx.Output, colors.Success("Reorder complete!\n"))
+	return nil, nil
+}
+
+type Continuation struct {
+	State *State
 }
